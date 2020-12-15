30bc7a81ceab1c6cb8cdd78e26f0103652627402
Tyrence <email1@jkbff.com>
2020-12-14 20:55:09 -0700

    initial commit

---
+++ src/ps2_online/core.clj
@@ -0,0 +1,6 @@
+(ns ps2-online.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/ps2_online/core_test.clj
@@ -0,0 +1,7 @@
+(ns ps2-online.core-test
+  (:require [clojure.test :refer :all]
+            [ps2-online.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
